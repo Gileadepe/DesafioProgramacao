20bfbd2dfa0b772cc529221e04c619687279c1d2
Gileadepe <gil_eade@hotmail.com>
2022-02-18 15:45:45 -0300

    correção da segunda questão

--- src/questoes/questao2/Questao2.java
+++ src/questoes/questao2/Questao2.java
@@ -1,13 +1,20 @@
 package questoes.questao2;
 
+import java.util.Scanner;
+
 public class Questao2 {
     public static void main(String[] args) {
       /* Uma senha segura é necessário vários critérios, porém a questão pediu que fosse
          informada quantidade mínima de caracters para que uma senha fosse segura */
-        String senha = "Ya3";
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma senha que tenha no mínimo 6 caracteres: ");
+        String senha = scan.next();
+
+        System.out.println("Quantidade de carcteres da senha criada: " +senha.length());
 
         if(senha.length()<6){
-            System.out.println("Quantidade de carcteres da senha criada: " +senha.length());
             System.out.println("\nSenha insegura a senha deve ter no mínimo 6 caracteres!" +
                     "\n Favor criar uma nova senha com no mínimo 6 caracteres!");
         }else{
